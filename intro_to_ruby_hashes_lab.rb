2a6b4c12a4289cea53c99026ea570c66abac79cd
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-19 03:09:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,9 +10,17 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+<<<<<<< HEAD
    return my_word= {:railroads => {:pieces => 4,:names => {:reading_railroad => {},:pennsylvania_railroad =>{},:b_and_o_railroad => {},:shortline_railroad => {}},:rent_in_dollars =>{:one_piece_owned => 25,:two_pieces_owned=> 50,:three_pieces_owned => 100,:four_pieces_owned => 200 }}}
 end
 
 def monopoly_with_fourth_tier
    my_word= {:railroads => {:pieces => 4,:names => {:reading_railroad => {'mortgage_value' => '$100'},:pennsylvania_railroad =>{"mortgage_value" => '$200'},:b_and_o_railroad => {"mortgage_value" => '$400'},:shortline_railroad => {"mortgage_value" => '$800'}},:rent_in_dollars =>{:one_piece_owned => 25,:two_pieces_owned=> 50,:three_pieces_owned => 100,:four_pieces_owned => 200 }}}
+=======
+   return my_word= {:railroads => {:pieces => 4,:names => {:reading_railroad => {:mortgage_value => {}},:pennsylvania_railroad =>{},:b_and_o_railroad => {},:shortline_railroad => {}},:rent_in_dollars =>{:one_piece_owned => 25,:two_pieces_owned=> 50,:three_pieces_owned => 100,:four_pieces_owned => 200 }}}
+end
+
+def monopoly_with_fourth_tier
+   my_word= {:railroads => {:pieces => 4,:names => {:reading_railroad => {},:pennsylvania_railroad =>{},:b_and_o_railroad => {},:shortline_railroad => {}},:rent_in_dollars =>{:one_piece_owned => 25,:two_pieces_owned=> 50,:three_pieces_owned => 100,:four_pieces_owned => 200 }}}
+>>>>>>> 2eb40aba99ee7d03bfdd830ab2e5a6352aa9cad0
 end
